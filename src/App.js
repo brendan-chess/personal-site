367449894eed77e40e45a6a49299a88cb12a823f
brendan-chess <brendanchess25@gmail.com>
2021-11-03 20:35:27 -0700

    initial route setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import Article from "./components/Article/Article";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import Home from './components/Home/Home'
+import Article from './components/Article/Article'
 
 const App = () => {
   return (
-    <>
-      <Article />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
