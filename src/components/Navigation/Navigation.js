97d370faf048da32f6d172ab15d914bf4089e2dc
brendan-chess <brendanchess25@gmail.com>
2021-11-06 17:37:41 -0700

    started navigation animation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,15 +8,14 @@ const Navigation = () => {
   return (
     <div className='Navigation-container'>
       <div onClick={() => setExpanded(!expanded)}>menu</div>
-      {
-        expanded && 
+      <div className={expanded ? 'Navigation-list-container-expanded' : 'Navigation-list-container-collapsed'}>
         <ul className='Navigation-list' onClick={() => setExpanded(!expanded)}>
           <li><NavLink exact to='/'  className='Navigation-link' activeClassName='Navigation-link-active'>home</NavLink></li>
           <li><NavLink to='/articles' className='Navigation-link' activeClassName='Navigation-link-active'>articles</NavLink></li>
           <li><NavLink to='/resume' className='Navigation-link' activeClassName='Navigation-link-active'>resume</NavLink></li>
           <li><NavLink to='/contact' className='Navigation-link' activeClassName='Navigation-link-active'>contact</NavLink></li>
         </ul>
-      }
+      </div>
     </div>
   )
 }
